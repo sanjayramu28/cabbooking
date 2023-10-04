42ed4461293da9cf29cc71ed1096fe26327cc009
Sanjay <sanjayrajamani2003@gmail.com>
2023-10-04 09:47:03 +0530

    new Files

--- cab book/public/assests/js/cabbook.js
+++ cab book/public/assests/js/cabbook.js
@@ -1,2 +1,11 @@
-const btn=document.querySelector('.one way');
-console.log(btn);
+// const tele=document.getElementById('one-waytel');
+// document.getElementById('bttn').addEventListener('click',()=>
+// {
+//     const tel=tele.value;
+// })
+// console.log(tele.value);
+// const ht=document.getElementById('star');
+// ht.addEventListener('mouseover',()=>
+// {
+//     ht.style.z-index:'1';
+// })
